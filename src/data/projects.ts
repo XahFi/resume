a2eb898b5534a12adfa4c0c2cd6164c8541a2871
Shafi R <shafi.rafiq85@gmail.com>
2024-12-05 16:52:05 +1100

    updated projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,6 +5,12 @@ const PROJECTS = [
         src: '',
         desc: 'A simple resume website to give an idea about my experience, skills and education. The background was looking a bit too simple though, so I thought I might have some fun by making it simulate a rainy background, while learning a bit about three js in the process. This was built in React (TypeScript).'
     },
+    {
+        title: '3D Backgrounds',
+        link: '',
+        src: '',
+        desc: 'The 3D backgrounds for this resume site initially came from a 3d project I was working on. The rainy and windy backgrounds are part of it, but I plan to add more as I continue to learn about three JS.'
+    },
 ];
 
 export default PROJECTS;
